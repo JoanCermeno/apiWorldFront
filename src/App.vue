fb073d4435f0b9569a1ef343b4893077f027d841
Joan Gabriel <joangabriel684@gmail.com>
2024-07-06 00:00:08 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HeaderPrincipal from './components/header.vue'
+</script>
+
+<template>
+  <HeaderPrincipal></HeaderPrincipal>
+  <main class="dark:bg-slate-700 relative top-5">
+    <RouterView></RouterView>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/header.vue
@@ -0,0 +1,110 @@
+<script setup>
+import { ref } from "vue";
+
+let menuIsVisible = ref(false);
+</script>
+
+<template>
+  <header
+    class="sticky top-4 w-[90%] dark:bg-[rgba(1,65,129,0.25)] dark:shadow-lg dark:backdrop-blur-md dark:rounded dark:border dark:border-gray-200 mx-auto z-10 py-2 px-4 flex lg:flex-row flex-col justify-between align-items-center lg:align-items-start bg-[rgba(250,249,249,0.804)] shadow-lg backdrop-blur-md rounded border border-gray-200"
+  >
+    <section class="text-2xl">
+      <span class="texto-con-degradado">{ API Wolrd }</span> 🌎
+    </section>
+    <!-- section con los enlaces de interes  -->
+
+    <nav
+      class="flex lg:flex-row w-full lg:w-[40%] justify-evenly flex-col text-center h-100"
+      :class="{ ocultar: menuIsVisible }"
+    >
+      <ul
+        class="h-[50%] lg:h-full mx-auto justify-center flex flex-col lg:flex-row items-center gap-2 p-2"
+      >
+        <!-- Aplicamos Flexbox y centramos el contenido dentro de cada <li> -->
+        <li class="flex items-center justify-center">About</li>
+        <li class="flex items-center justify-center">About</li>
+        <li class="flex items-center justify-center">About</li>
+        <li class="flex items-center justify-center">About</li>
+      </ul>
+    </nav>
+
+    <div
+      class="lg:hidden absolute right-4 cursor-pointer"
+      @click="menuIsVisible = !menuIsVisible"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-7"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
+      </svg>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.texto-con-degradado {
+  font-size: 24px;
+  background-image: linear-gradient(to left, rgb(245, 152, 245), #6ddfff);
+  background-clip: text;
+  background-size: 200%;
+  -webkit-text-fill-color: transparent;
+  /* Fallback para navegadores que no soportan las propiedades anteriores */
+  color: #ffffff;
+  animation: moverGradient linear infinite 10s;
+}
+
+@keyframes moverGradient {
+  0% {
+    background-position: 0%;
+  }
+  50% {
+    background-position: 100%;
+  }
+  100% {
+    background-position: 10%;
+  }
+}
+
+li {
+  position: relative;
+  user-select: none;
+
+  cursor: pointer;
+}
+li::after {
+  content: "";
+  position: absolute;
+  width: 0%;
+  height: 2px;
+  bottom: 0px;
+  transition: 0.2s ease-out width;
+  background-color: white;
+}
+li:hover::after {
+  width: 100%;
+}
+/* transiion del menu */
+
+nav {
+  transition: all 1s linear;
+  max-height: 200px;
+  max-width: 100%;
+  overflow: hidden;
+}
+
+.ocultar {
+  overflow: hidden;
+  max-height: 0px;
+
+  transition: all 2s;
+}
+</style>
