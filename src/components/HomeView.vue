2338ec7a19e4cd1be64269d9a345e319d2a1f4b8
Joan Gabriel <joangabriel684@gmail.com>
2024-09-27 23:56:40 -0400

    main form adapte to mobile device layaout

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -26,14 +26,11 @@ const showMap = (
   <section
     class="container mx-auto flex flex-col gap-4 py-4 w-[90%] my-12 max-w-4xl"
   >
-    <h1 class="text-5xl font-bold text-center text-white">
-      Welcome to { API World } 🌎
-    </h1>
     <p class="text-white text-md p-4 text-center">
       ¡Hola! 👋 aqui tienes una lista completa de todos los países del mundo,
       detallando sus estados o regiones, así como sus ciudades o pueblos. Listo
       para ser usado en cualquier proyecto Colaboradores:👨🏻‍💻
-      <b>Joan Cermeño</b> y <b>Luis Viera</b> 🇻🇪
+      <b>Joan Cermeño</b> y <b>Todo el que quiera participar!</b> 🇻🇪
     </p>
   </section>
   <mainForm @mostrar-mapa="showMap"></mainForm>
